51d05430c41d4a12a2581003ce5efbb694506ebf
Arthur Benware <artpolitic91@gmail.com>
2022-09-08 22:44:24 -0400

    commit tree 739965c05e017ec81d61359cb2179b61746e57a9

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42) {
+        return (someValue -42)
+    }
+    else {
+        return 42 - someValue
+    }
+  }
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) *264
+    }
+    else {
+        return (start -destination) *264
+    }
+}
+function calculatesFarePrice (start, destination) {
+   if (start - destination < 400) {
+    return 0
+   }
+   else if (start - destination > 2500) {
+    return 'cannot travel that far'
+   }
+}
